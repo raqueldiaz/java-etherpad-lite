0b7492e403d3d8ed2879cfe707e51b9923a0726d
raquel.diaz.otero <raquel.diaz.otero@udc.es>
2018-12-18 05:05:54 +0100

    Tests JUnit-QuickCheck

--- src/test/java/net/gjerull/etherpad/client/EPLiteConnectionPropertiesTest.java
+++ src/test/java/net/gjerull/etherpad/client/EPLiteConnectionPropertiesTest.java
@@ -2,6 +2,7 @@ package net.gjerull.etherpad.client;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -22,6 +23,10 @@ public class EPLiteConnectionPropertiesTest {
     /** The Constant ENCODING. */
     private static final String ENCODING = "UTF-8";
 
+    /** The Constant RESPONSE_TEMPLATE. */
+    private static final String RESPONSE_TEMPLATE = "{\n" + "  \"code\": %d,\n"
+            + "  \"message\": \"%s\",\n" + "  \"data\": %s\n" + "}";
+
     /**
      * Domain with trailing slash when construction an api path.
      *
@@ -29,11 +34,10 @@ public class EPLiteConnectionPropertiesTest {
      * @throws Exception the exception
      */
     @Property
-    public void domain_with_trailing_slash_when_construction_an_api_path(
+    public void domain_without_trailing_slash_when_construction_an_api_path(
             String exampleMethod) throws Exception {
-
-        EPLiteConnection connection = new EPLiteConnection(
-                "http://example.com/", "apikey", API_VERSION, ENCODING);
+        EPLiteConnection connection = new EPLiteConnection("http://example.com",
+                "apikey", API_VERSION, ENCODING);
         String apiMethodPath = connection.apiPath(exampleMethod);
         assertEquals("/api/1.2.12/" + exampleMethod, apiMethodPath);
     }
@@ -56,5 +60,28 @@ public class EPLiteConnectionPropertiesTest {
         String queryString = connection.queryString(apiArgs, false);
 
         assertEquals("apikey=apikey&padID=" + padID + "&rev=27", queryString);
+    }
+
+    /**
+     * Handle valid response from server.
+     *
+     * @param test1 the test 1
+     * @param test2 the test 2
+     * @param test3 the test 3
+     * @throws Exception the exception
+     */
+    @Property
+    public void handle_valid_response_from_server(String test1, String test2,
+            String test3) throws Exception {
+        EPLiteConnection connection = new EPLiteConnection(
+                "http://example.com/", "apikey", API_VERSION, ENCODING);
+        String listAllPads = "{\"padIDs\": [\n" + "  \"" + test1 + "\",\n"
+                + "  \"" + test2 + "\",\n" + "  \"" + test3 + "\"\n" + "]}";
+        String serverResponse = String.format(RESPONSE_TEMPLATE, 0,
+                "no or wrong API Key", listAllPads);
+
+        Map response = (Map) connection.handleResponse(serverResponse);
+
+        assertEquals(test1, ((List) response.get("padIDs")).get(0));
     }
 }
